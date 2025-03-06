b39961f4b4bcad7c98ecb5e76a99fbe2abc91924
bmr infotech <bmr_client@outlook.com>
2025-03-06 21:09:10 +0530

    Create A GroupLedger Form

---
+++ eTactwebAccounts/Controllers/AccGroupLedgerController.cs
@@ -0,0 +1,61 @@
+﻿using eTactWeb.Data.Common;
+using eTactWeb.DOM.Models;
+using eTactWeb.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+
+namespace eTactwebAccounts.Controllers
+{
+    public class AccGroupLedgerController : Controller
+    {
+        private readonly IDataLogic _IDataLogic;
+        public IAccGroupLedger _IAccGroupLedger { get; }
+
+        private readonly ILogger<AccGroupLedgerController> _logger;
+        private readonly IConfiguration iconfiguration;
+        private readonly IMemoryCache _MemoryCache;
+        public IWebHostEnvironment _IWebHostEnvironment { get; }
+        public AccGroupLedgerController(ILogger<AccGroupLedgerController> logger, IDataLogic iDataLogic, IAccGroupLedger iAccGroupLedger, IMemoryCache iMemoryCache, EncryptDecrypt encryptDecrypt, IWebHostEnvironment iWebHostEnvironment, IConfiguration iconfiguration)
+        {
+            _logger = logger;
+            _IDataLogic = iDataLogic;
+            _IAccGroupLedger = iAccGroupLedger;
+            _MemoryCache = iMemoryCache;
+            _IWebHostEnvironment = iWebHostEnvironment;
+            this.iconfiguration = iconfiguration;
+        }
+        [Route("{controller}/Index")]
+        [HttpGet]
+        public async Task<ActionResult> AccGroupLedger()
+        {
+            var MainModel = new AccGroupLedgerModel();
+            MainModel.AccGroupLedgerGrid = new List<AccGroupLedgerModel>();
+            MainModel.FromDate = HttpContext.Session.GetString("FromDate");
+            MainModel.ToDate = HttpContext.Session.GetString("ToDate");
+            
+
+            return View(MainModel); // Pass the model with old data to the view
+        }
+        public async Task<JsonResult> FillGroupName(string FromDate, string ToDate)
+        {
+            var JSON = await _IAccGroupLedger.FillGroupName( FromDate,ToDate);
+            string JsonString = JsonConvert.SerializeObject(JSON);
+            return Json(JsonString);
+        }
+        public async Task<IActionResult> GetGroupLedgerDetailsData(string FromDate, string ToDate, int GroupCode, string ReportType)
+        {
+            var model = new AccGroupLedgerModel();
+            model = await _IAccGroupLedger.GetGroupLedgerDetailsData(FromDate, ToDate, GroupCode, ReportType);
+            if (ReportType == "PRIMARYGROUPSUMMARY")
+            {
+                return PartialView("_GroupLedgerSummaryGrid", model);
+            }
+            if (ReportType == "PRIMARYGROUPdetail")
+            {
+                return PartialView("_GroupLedgerDetailGrid", model);
+            }
+            return null;
+        }
+    }
+}
