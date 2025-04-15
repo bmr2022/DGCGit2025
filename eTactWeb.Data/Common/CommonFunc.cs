726ab83896ef868d531c8816ccd66f9e466d5479
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-04-15 09:46:16 +0530

    add date Format

--- eTactWeb.Data/Common/CommonFunc.cs
+++ eTactWeb.Data/Common/CommonFunc.cs
@@ -148,7 +148,7 @@ public static class CommonFunc
              "yyyy-MM-ddTHH:mm:ss","yyyy-MM-ddTHH:mm:ssZ","yyyy-MM-ddTHH:mm:ss.SSS",
              "yyyy-MM-ddTHH:mm:ss.SSSZ","yyyy-MM-ddTHH:mm:ss+HH:mm","EEEE, MMMM dd, yyyy",
              "EEE, dd MMM yyyy","EEEE dd/MM/yyyy","EEEE dd-MMM-yyyy","HH:mm:ss","hh:mm:ss a","dd-MMM-yyyy hh:mm:ss tt",
-             "HH:mm","hh:mm a","dd/MMM/yyyy hh:mm:ss tt", "dd/MMM/yyyy hh:mm:ss"
+             "HH:mm","hh:mm a","dd/MMM/yyyy hh:mm:ss tt", "dd/MMM/yyyy hh:mm:ss","dd/MM/yyyy hh:mm:ss tt","dd-MM-yyyy hh:mm:ss","dd-MM-yyyy HH:mm:ss"
         };
 
             // Try parsing the date string against all the formats
