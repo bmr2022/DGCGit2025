c4f7f57363b19faeeaa88c530a0323365d3a865f
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-04-21 12:22:56 +0530

    PO--change in date IssThrBOM--add rights

--- eTactWeb.Data/DAL/IssueThrBOMDAL.cs
+++ eTactWeb.Data/DAL/IssueThrBOMDAL.cs
@@ -29,6 +29,28 @@ namespace eTactWeb.Data.DAL
             DBConnectionString = configuration.GetConnectionString("eTactDB");
         }
 
+        public async Task<ResponseResult> GetFormRights(int userID)
+        {
+            var _ResponseResult = new ResponseResult();
+            try
+            {
+                var SqlParams = new List<dynamic>();
+                SqlParams.Add(new SqlParameter("@Flag", "GetRights"));
+                SqlParams.Add(new SqlParameter("@EmpId", userID));
+                SqlParams.Add(new SqlParameter("@MainMenu", "Material to Issue Thr BOM"));
+                //SqlParams.Add(new SqlParameter("@SubMenu", "Sale Order"));
+
+                _ResponseResult = await _IDataLogic.ExecuteDataSet("SP_ItemGroup", SqlParams);
+            }
+            catch (Exception ex)
+            {
+                dynamic Error = new ExpandoObject();
+                Error.Message = ex.Message;
+                Error.Source = ex.Source;
+            }
+            return _ResponseResult;
+        }
+
         public async Task<ResponseResult> GetNewEntry(int YearCode)
         {
             var _ResponseResult = new ResponseResult();
