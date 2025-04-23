6e7aaf20c1ac4047d34ad2383dd4a6775329bdf7
bmr infotech <bmr_client@outlook.com>
2025-04-23 15:15:45 +0530

    Create RecChallanReport

---
+++ eTactWeb.DOM/Models/RecChallanReportModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static eTactWeb.DOM.Models.Common;
+
+namespace eTactWeb.DOM.Models
+{
+    public class RecChallanReportModel:TimeStamp
+    {
+        public int EntryId { get; set; }
+        public int YearCode { get; set; }
+        public string RCMChallanNo { get; set; }
+        public string RetNonRetChallan { get; set; }
+        public string GateNo { get; set; }
+        public string? GateDate { get; set; }
+        public string BillChallan { get; set; }
+        public string ChallanNo { get; set; }
+        public string? ChallanDate { get; set; }
+        public string PartyName { get; set; }
+        public string ChallanType { get; set; }
+        public string MRNNo { get; set; }
+        public string? MRNDate { get; set; }
+        public string PartCode { get; set; }
+        public string ItemName { get; set; }
+        public decimal GateQty { get; set; }
+        public decimal RecQty { get; set; }
+        public decimal Rate { get; set; }
+        public decimal Amount { get; set; }
+        public string IssueChallanNo { get; set; }
+        public int IssueChallanYearCode { get; set; }
+        public decimal IssuedQty { get; set; }
+        public string Remark { get; set; }
+        public string RecInStore { get; set; }
+        public string DocTypeCode { get; set; }
+        public string DocName { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public IList<RecChallanReportModel> RecChallanReportGrid { get; set; }
+    }
+}
---
+++ eTactWeb.Data/BLL/RecChallanReportBLL.cs
@@ -0,0 +1,30 @@
+﻿using eTactWeb.Data.Common;
+using eTactWeb.Data.DAL;
+using eTactWeb.DOM.Models;
+using eTactWeb.Services.Interface;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTactWeb.Data.BLL
+{
+    public class RecChallanReportBLL : IRecChallanReport
+    {
+        private RecChallanReportDAL _RecChallanReportDAL;
+        private readonly IDataLogic _DataLogicDAL;
+
+        public RecChallanReportBLL(IConfiguration config, IDataLogic dataLogicDAL, ConnectionStringService connectionStringService)
+        {
+            _RecChallanReportDAL = new RecChallanReportDAL(config, dataLogicDAL, connectionStringService);
+            _DataLogicDAL = dataLogicDAL;
+        }
+        public async Task<RecChallanReportModel> GetInventoryAgingReportDetailsData(string fromDate, string toDate, int EntryId, int YearCode)
+        {
+            return await _RecChallanReportDAL.GetInventoryAgingReportDetailsData( fromDate,  toDate,  EntryId,  YearCode);
+        }
+    }
+   
+}
