260962b1e5b4ab7e54f92806849cfb4fc7202092
xon <bmr.client2021@gmail.com>
2025-05-08 01:11:44 +0530

    Add AdjustAmount feature and enhance UI elements
    - Introduced `AdjustAmount` property in `BankReceiptModel.cs`.
    - Removed unnecessary financial input fields in `BankReceipt.cshtml`.
    - Added `AdjustAmount` input field in the pending vouchers modal.
    - Implemented `infoDialog` modal for contextual help.
    - Created `ChangeAdjustAmount()` function for dynamic adjustments.
    - Updated dropdown and input fields in `_BankReceipt.cshtml` with a new background color for better visibility.
    - Enhanced `DRCR` display in `_BankReceiptGrid.cshtml` with a background color for clarity.

--- eTactWeb.DOM/Models/BankReceiptModel.cs
+++ eTactWeb.DOM/Models/BankReceiptModel.cs
@@ -142,6 +142,7 @@ namespace eTactWeb.DOM.Models
         public string Searchbox { get; set; }
         public string? DashboardType {  get; set; }
         public decimal PopUpBalance {  get; set; }
+        public decimal AdjustAmount { get; set; }
         public decimal PopDrAmt { get; set; }
         public decimal PopCrAmt { get; set; }
         public decimal TotalPopDrAmt { get; set; }
