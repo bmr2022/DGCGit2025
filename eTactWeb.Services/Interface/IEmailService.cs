6d9bbf90bff5ed71f4a360cead706875c4ee66c6
bmr2022 <105047305+bmr2022@users.noreply.github.com>
2025-06-11 08:46:40 +0530

    email

---
+++ eTactWeb.Services/Interface/IEmailService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTactWeb.Services.Interface
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(
+            string emailTo,
+            string subject,
+            string message,
+            byte[] attachment = null,
+            string attachmentName = "Report.pdf");
+    }
+}
