979c51bd5a78b5daccfe5a015a994279529b3080
xon <bmr.client2021@gmail.com>
2025-06-13 11:12:03 +0530

    einvoice

---
+++ eTactWeb.DOM/Models/EInvoiceItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTactWeb.DOM.Models
+{
+    public class EInvoiceItemModel
+    {
+        public int EntryId { get; set; }
+        public string InvoiceNo { get; set; }
+        public int YearCode { get; set; }
+        public string saleBillType { get; set; }
+        public string customerPartCode { get; set; }
+    }
+}
