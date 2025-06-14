446d878eef2fccc52b9c76de050510a1f8feb8c0
xon <bmr.client2021@gmail.com>
2025-06-14 23:57:20 +0530

    Add ShowAll feature and improve validation logic
    - Introduced `ShowAll` property in `BankPaymentModel` and updated related methods in `BankPaymentDAL` and `BankPaymentController`.
    - Added a checkbox for `ShowAll` in `BankPayment.cshtml` and adjusted JavaScript functions to handle new functionality.
    - Refactored `Validate_Submit` in `JournalVoucher.cshtml` to ensure unique ledger names.
    - Enhanced `ChangeAdjustAmount` function across multiple views for better validation and adjustment handling.
    - Updated `CheckUnCheckEdit` to manage row selection based on existing voucher IDs.
    - Improved JavaScript structure and added checks for required fields to enhance user experience.

--- eTactwebAccounts/Controllers/BankPaymentController.cs
+++ eTactwebAccounts/Controllers/BankPaymentController.cs
@@ -338,9 +338,9 @@ namespace eTactwebAccounts.Controllers
             string JsonString = JsonConvert.SerializeObject(JSON);
             return Json(JsonString);
         }
-        public async Task<JsonResult> FillLedgerName(string VoucherType, string Type)
+        public async Task<JsonResult> FillLedgerName(string VoucherType, string ShowAll)
         {
-            var JSON = await _IBankPayment.FillLedgerName(VoucherType, Type);
+            var JSON = await _IBankPayment.FillLedgerName(VoucherType, ShowAll);
             string JsonString = JsonConvert.SerializeObject(JSON);
             return Json(JsonString);
         }
