243ab9c321de73de31fbc677a7318fd6faff6e16
xon <bmr.client2021@gmail.com>
2025-06-15 11:12:27 +0530

    Change in FillLedger Flag

--- eTactWeb.Data/DAL/CashPaymentDAL.cs
+++ eTactWeb.Data/DAL/CashPaymentDAL.cs
@@ -80,7 +80,7 @@ namespace eTactWeb.Data.DAL
                 var SqlParams = new List<dynamic>();
                 SqlParams.Add(new SqlParameter("@Flag", "FillLedger"));
                 SqlParams.Add(new SqlParameter("@VoucherType", "Cash-Payment"));
-                SqlParams.Add(new SqlParameter("@type", Type));
+                SqlParams.Add(new SqlParameter("@ShowAllLedger", Type));
                 _ResponseResult = await _IDataLogic.ExecuteDataTable("AccSpVoucherEntry", SqlParams);
             }
             catch (Exception ex)
