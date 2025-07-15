20879a24aedc460a3b8148a9ff87b077c92e72c1
bmr infotech <bmr_client@outlook.com>
2025-07-16 00:37:26 +0530

    Change for Dashboard chart and Designs

---
+++ eTactWeb.Services/Interface/IPdfService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTactWeb.Services.Interface
+{
+    public interface IPdfService
+    {
+        byte[] GeneratePdfFromHtml(string html);
+    }
+}
---
+++ eTactWeb.Services/PdfService.cs
@@ -0,0 +1,43 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using eTactWeb.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTactWeb.Services
+{
+    public   class PdfService : IPdfService
+    {
+        private readonly IConverter _converter;
+
+        public PdfService(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        public byte[] GeneratePdfFromHtml(string html)
+        {
+            var document = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                PaperSize = PaperKind.A4,
+                Orientation = Orientation.Portrait,
+            },
+                Objects = {
+                new ObjectSettings() {
+                    HtmlContent = html,
+                    WebSettings = {
+                        DefaultEncoding = "utf-8",
+                        LoadImages = true
+                    }
+                }
+            }
+            };
+
+            return _converter.Convert(document);
+        }
+    }
+}
