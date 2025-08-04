4e088aa094c0e00ab55cfb68973690276fa0afdf
bmr infotech <bmr_client@outlook.com>
2025-08-04 16:27:39 +0530

    Add Save Func In DiscountCustomerCategoryMaster

--- eTactWeb.Data/DAL/DiscountCustomerCategoryMasterDAL.cs
+++ eTactWeb.Data/DAL/DiscountCustomerCategoryMasterDAL.cs
@@ -1,4 +1,5 @@
 ﻿using eTactWeb.Data.Common;
+using eTactWeb.DOM.Models;
 using eTactWeb.Services.Interface;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -41,5 +42,185 @@ namespace eTactWeb.Data.DAL
 
 			return _ResponseResult;
 		}
-	}
+		public async Task<ResponseResult> FillDiscountCategory()
+		{
+			var _ResponseResult = new ResponseResult();
+			try
+			{
+				var SqlParams = new List<dynamic>();
+				SqlParams.Add(new SqlParameter("@Flag", "FillDiscountCategory"));
+				_ResponseResult = await _IDataLogic.ExecuteDataTable("SPSalesDiscountCustomerCategoryMaster", SqlParams);
+			}
+			catch (Exception ex)
+			{
+				dynamic Error = new ExpandoObject();
+				Error.Message = ex.Message;
+				Error.Source = ex.Source;
+			}
+
+			return _ResponseResult;
+		}
+
+
+		public async Task<ResponseResult> SaveDiscountCustomerCategoryMaster(DiscountCustomerCategoryMasterModel model)
+		{
+			var _ResponseResult = new ResponseResult();
+
+			try
+			{
+				var sqlParams = new List<dynamic>();
+
+				if (model.Mode == "U" || model.Mode == "V")
+				{
+					sqlParams.Add(new SqlParameter("@Flag", "UPDATE"));
+					//sqlParams.Add(new SqlParameter("@DiscountCustCatEntryId", model.DiscountCustCatEntryId));
+
+					sqlParams.Add(new SqlParameter("@LastUpdatedbyEmpId", model.LastUpdatedbyEmpId));
+					sqlParams.Add(new SqlParameter("@LastupDationDate", model.LastupDationDate));
+				}
+				else
+				{
+					sqlParams.Add(new SqlParameter("@Flag", "INSERT"));
+					sqlParams.Add(new SqlParameter("@DiscountCustCatEntryId", model.DiscountCustCatEntryId));
+				}
+					
+					sqlParams.Add(new SqlParameter("@DiscountCustCatYearCode", model.DiscountCustCatYearCode));
+					sqlParams.Add(new SqlParameter("@EffectiveFromDate", model.EffectiveFromDate));
+					sqlParams.Add(new SqlParameter("@DiscountCategory", model.DiscountCategory));
+					sqlParams.Add(new SqlParameter("@DiscountCatSlipNo", model.DiscountCatSlipNo));
+					sqlParams.Add(new SqlParameter("@MaxDiscountPer", model.MaxDiscountPer));
+					sqlParams.Add(new SqlParameter("@MinDiscountPer", model.MinDiscountPer));
+					sqlParams.Add(new SqlParameter("@ApplicableOnAdvancePayment", model.ApplicableOnAdvancePayment));
+					sqlParams.Add(new SqlParameter("@MinmumAdvancePaymentPercent", model.MinmumAdvancePaymentPercent));
+					sqlParams.Add(new SqlParameter("@CategoryActive", model.CategoryActive));
+					sqlParams.Add(new SqlParameter("@ApplicableMonthlyYearlyAfterEachSale", model.ApplicableMonthlyYearlyAfterEachSale));
+					
+					sqlParams.Add(new SqlParameter("@ActualEntryByEmpId", model.ApprovedByEmpId));
+					sqlParams.Add(new SqlParameter("@ActualEntryDate", model.ActualEntryDate));
+					sqlParams.Add(new SqlParameter("@EntryByMachine", model.EntryByMachine));
+					sqlParams.Add(new SqlParameter("@CC", model.CC));
+					sqlParams.Add(new SqlParameter("@ApprovedByEmpId", model.ApprovedByEmpId));
+				
+
+				_ResponseResult = await _IDataLogic.ExecuteDataTable("SPSalesDiscountCustomerCategoryMaster", sqlParams);
+			}
+			catch (Exception ex)
+			{
+				_ResponseResult.StatusCode = HttpStatusCode.InternalServerError;
+				_ResponseResult.StatusText = "Error";
+				_ResponseResult.Result = new { ex.Message, ex.StackTrace };
+			}
+
+			return _ResponseResult;
+		}
+		public async Task<ResponseResult> GetFormRights(int userID)
+		{
+			var _ResponseResult = new ResponseResult();
+			try
+			{
+				var SqlParams = new List<dynamic>();
+				SqlParams.Add(new SqlParameter("@Flag", "GetRights"));
+				SqlParams.Add(new SqlParameter("@EmpId", userID));
+				SqlParams.Add(new SqlParameter("@MainMenu", "Discount Customer Category Master"));
+				//SqlParams.Add(new SqlParameter("@SubMenu", "Sale Order"));
+
+				_ResponseResult = await _IDataLogic.ExecuteDataSet("SP_ItemGroup", SqlParams);
+			}
+			catch (Exception ex)
+			{
+				dynamic Error = new ExpandoObject();
+				Error.Message = ex.Message;
+				Error.Source = ex.Source;
+			}
+			return _ResponseResult;
+		}
+        public async Task<ResponseResult> GetDashboardData(DiscountCustomerCategoryMasterModel model)
+        {
+            var _ResponseResult = new ResponseResult();
+            try
+            {
+                var SqlParams = new List<dynamic>();
+                SqlParams.Add(new SqlParameter("@Flag", "DASHBOARD"));
+                SqlParams.Add(new SqlParameter("@FromDate", model.FromDate));
+                SqlParams.Add(new SqlParameter("@ToDate", model.ToDate));
+
+                _ResponseResult = await _IDataLogic.ExecuteDataSet("SPSalesDiscountCustomerCategoryMaster", SqlParams);
+            }
+            catch (Exception ex)
+            {
+                dynamic Error = new ExpandoObject();
+                Error.Message = ex.Message;
+                Error.Source = ex.Source;
+            }
+            return _ResponseResult;
+        }
+        public async Task<DiscountCustomerCategoryMasterModel> GetDashboardDetailData(string FromDate, string ToDate)
+        {
+            DataSet? oDataSet = new DataSet();
+            var model = new DiscountCustomerCategoryMasterModel();
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(DBConnectionString))
+                {
+                    SqlCommand oCmd = new SqlCommand("SPSalesDiscountCustomerCategoryMaster", myConnection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    oCmd.Parameters.AddWithValue("@Flag", "DASHBOARD");
+                    oCmd.Parameters.AddWithValue("@Fromdate", FromDate);
+                    oCmd.Parameters.AddWithValue("@Todate", ToDate);
+
+                    await myConnection.OpenAsync();
+                    using (SqlDataAdapter oDataAdapter = new SqlDataAdapter(oCmd))
+                    {
+                        oDataAdapter.Fill(oDataSet);
+                    }
+                }
+                if (oDataSet.Tables.Count > 0 && oDataSet.Tables[0].Rows.Count > 0)
+                {
+                    
+                        model.DiscountCustomerCategoryMasterGrid = (from DataRow dr in oDataSet.Tables[0].Rows
+                                                        select new DiscountCustomerCategoryMasterModel
+                                                        {
+                                                            DiscountCustCatEntryId = dr["DiscountCustCatEntryId"] != DBNull.Value ? Convert.ToInt32(dr["DiscountCustCatEntryId"]) : 0,
+                                                            DiscountCustCatYearCode = dr["DiscountCustCatYearCode"] != DBNull.Value ? Convert.ToInt32(dr["DiscountCustCatYearCode"]) : 0,
+                                                            DiscountCategory = dr["DiscountCategory"] != DBNull.Value ? Convert.ToString(dr["DiscountCategory"]) : string.Empty,
+                                                            DiscountCatSlipNo = dr["DiscountCatSlipNo"] != DBNull.Value ? Convert.ToString(dr["DiscountCatSlipNo"]) : string.Empty,
+                                                            EffectiveFromDate = dr["EffectiveFromDate"] != DBNull.Value ? Convert.ToDateTime(dr["EffectiveFromDate"]).ToString("dd/MM/yyyy") : string.Empty,
+                                                            MinDiscountPer = dr["MinDiscountPer"] != DBNull.Value ? Convert.ToDecimal(dr["MinDiscountPer"]) : 0,
+                                                            MaxDiscountPer = dr["MaxDiscountPer"] != DBNull.Value ? Convert.ToDecimal(dr["MaxDiscountPer"]) : 0,
+                                                            ApplicableMonthlyYearlyAfterEachSale = dr["ApplicableMonthlyYearlyAfterEachSale"] != DBNull.Value ? Convert.ToString(dr["ApplicableMonthlyYearlyAfterEachSale"]) : string.Empty,
+                                                            ApplicableOnAdvancePayment = dr["ApplicableOnAdvancePayment"] != DBNull.Value ? Convert.ToString(dr["ApplicableOnAdvancePayment"]) : string.Empty,
+                                                            MinmumAdvancePaymentPercent = dr["MinmumAdvancePaymentPercent"] != DBNull.Value ? Convert.ToDecimal(dr["MinmumAdvancePaymentPercent"]) : 0,
+                                                            
+                                                            CategoryActive = dr["CategoryActive"] != DBNull.Value ? Convert.ToString(dr["CategoryActive"]) : string.Empty,
+                                                            EntryByMachine = dr["EntryByMachine"] != DBNull.Value ? Convert.ToString(dr["EntryByMachine"]) : string.Empty,
+                                                            ActualEntryByEmpId = dr["ActualEntryByEmpId"] != DBNull.Value ? Convert.ToInt32(dr["ActualEntryByEmpId"]) : 0,
+                                                            ActualEntryByEmpName = dr["ActualEmployee"] != DBNull.Value ? Convert.ToString(dr["ActualEmployee"]) : string.Empty,
+                                                            ActualEntryDate = dr["ActualEntryDate"] != DBNull.Value ? Convert.ToDateTime(dr["ActualEntryDate"]).ToString("dd/MM/yyyy") : string.Empty,
+                                                            LastUpdatedbyEmpId = dr["LastUpdatedbyEmpId"] != DBNull.Value ? Convert.ToInt32(dr["LastUpdatedbyEmpId"]) : 0,
+                                                            LastUpdatedbyEmpName = dr["UpdatedByEmployee"] != DBNull.Value ? Convert.ToString(dr["UpdatedByEmployee"]) : string.Empty,
+                                                            LastupDationDate = dr["LastupDationDate"] != DBNull.Value ? Convert.ToDateTime(dr["LastupDationDate"]).ToString("dd/MM/yyyy") : string.Empty,
+                                                            CC = dr["CC"] != DBNull.Value ? Convert.ToString(dr["CC"]) : string.Empty,
+                                                            ApprovedByEmpId = dr["ApprovedByEmpId"] != DBNull.Value ? Convert.ToInt32(dr["ApprovedByEmpId"]) : 0,
+
+                                                        }).ToList();
+                    
+
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                dynamic Error = new ExpandoObject();
+                Error.Message = ex.Message;
+                Error.Source = ex.Source;
+            }
+            finally
+            {
+                oDataSet.Dispose();
+            }
+            return model;
+        }
+    }
 }
