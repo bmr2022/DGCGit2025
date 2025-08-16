fdba61a8b23c1aafc5d58bc80f05a51b9513fa10
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-08-16 15:09:09 +0530

    rights

--- eTactWeb.Data/DAL/StoreMasterDAL.cs
+++ eTactWeb.Data/DAL/StoreMasterDAL.cs
@@ -25,6 +25,27 @@ namespace eTactWeb.Data.DAL
             _connectionStringService = connectionStringService;
             DBConnectionString = _connectionStringService.GetConnectionString();
         }
+        public async Task<ResponseResult> GetFormRights(int userId)
+        {
+            var _ResponseResult = new ResponseResult();
+            try
+            {
+                var SqlParams = new List<dynamic>();
+                SqlParams.Add(new SqlParameter("@Flag", "GetRights"));
+                SqlParams.Add(new SqlParameter("@EmpId", userId));
+                SqlParams.Add(new SqlParameter("@MainMenu", "Store Master"));
+
+                _ResponseResult = await _IDataLogic.ExecuteDataSet("SP_ItemGroup", SqlParams);
+            }
+            catch (Exception ex)
+            {
+                dynamic Error = new ExpandoObject();
+                Error.Message = ex.Message;
+                Error.Source = ex.Source;
+            }
+            return _ResponseResult;
+        }
+
         public async Task<ResponseResult> FillStoreType()
         {
             var _ResponseResult = new ResponseResult();
