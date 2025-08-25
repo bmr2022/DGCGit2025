bc73052c919b0488bc5b2e957dd327ecda71ae2a
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-08-25 22:30:47 +0530

    CRUD Operation of CustomerDiscountCategory

--- eTactWeb/Startup.cs
+++ eTactWeb/Startup.cs
@@ -276,6 +276,7 @@ namespace eTactWeb
             services.TryAddTransient<IWorkCenterMaster, WorkCenterMasterBLL>();
             services.TryAddTransient<IMachineMaster, MachineMasterBLL>();
             services.TryAddTransient<IMRNRegister, MRNRegisterBLL>();
+            services.TryAddScoped<ICustomerDiscountCategory, CustomerDiscountCategoryBLL>();
             services.TryAddTransient<IMIRRegister, MIRRegisterBLL>();
             services.TryAddTransient<ICreditNote, CreditNoteBLL>();
             services.TryAddTransient<IBankReconciliation, BankReconciliationBLL>();
