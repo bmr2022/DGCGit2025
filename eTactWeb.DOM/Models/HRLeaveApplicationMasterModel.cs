8fa3a92248eba1c31003e8aa6d40c4ee5faa1978
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-08-29 19:39:18 +0530

    hrleaveapplication

--- eTactWeb.DOM/Models/HRLeaveApplicationMasterModel.cs
+++ eTactWeb.DOM/Models/HRLeaveApplicationMasterModel.cs
@@ -17,8 +17,8 @@ namespace eTactWeb.DOM.Models
         public string? ApplicationNo { get; set; }
         public string? ApplicationDate { get; set; }
         public int? EmpId { get; set; }
-        public string? EmpName { get; set; }
-        public string? EmpCode { get; set; }
+        public string  EmpName { get; set; }
+        public string EmpCode { get; set; }
         public string? MobileNo { get; set; }
         public string? PhoneNo { get; set; }
         public int? desigEntryId { get; set; }
@@ -87,7 +87,7 @@ namespace eTactWeb.DOM.Models
         public int DeptId { get; set; }
         public string? Department { get; set; }
         public int ShiftId { get; set; }
-        public string? Shift { get; set; }
+        public string  Shift { get; set; }
         public decimal? TotalYearlyLeave { get; set; }
         public decimal? TotalMonthlyLeave { get; set; }
 
