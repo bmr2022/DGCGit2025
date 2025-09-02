edc238b7fbcc46a366f8067ed6000d6cbfc4a819
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-09-02 23:15:28 +0530

    put validation in hsn no and make dropdown for item and service

--- eTactWeb.DOM/Models/HSNMasterModel.cs
+++ eTactWeb.DOM/Models/HSNMasterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace eTactWeb.DOM.Models
     public class HSNMasterModel
     {
         public int HSNEntryID { get; set; }
+
+        [Required(ErrorMessage = "HSN No is required")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "HSN No must be exactly 8 digits (numbers only)")]
         public string HSNNo { get; set; }
+        [Required(ErrorMessage = "HSN Type is required")]
         public string HSNTypeItemServ { get; set; }
         public string EntryByMachineName { get; set; }
         public int ActualEntryBy { get; set; }
