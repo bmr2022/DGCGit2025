6e44b8a8ed882dba5f57e831c57e9f7604ff9771
bmr infotech <bmr_client@outlook.com>
2025-09-13 16:06:42 +0530

    changes in controlplan

---
+++ eTactWeb.Data/BLL/ScheduleCalibrationBLL.cs
@@ -0,0 +1,13 @@
+﻿using eTactWeb.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTactWeb.Data.BLL
+{
+    public  class ScheduleCalibrationBLL:IScheduleCalibration
+    {
+    }
+}
---
+++ eTactWeb/Controllers/ScheduleCalibrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eTactWeb.Controllers
+{
+    public class ScheduleCalibrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
