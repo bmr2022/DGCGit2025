b7a931403ea34c2f55937638cb70a1f030d06bcc
bmr infotech <bmr_client@outlook.com>
2025-09-18 19:10:45 +0530

    Change in SaleInvoice Setting

---
+++ eTactWeb/Controllers/ControllerExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.IO;
+using System.Threading.Tasks;
+
+public static class ControllerExtensions
+{
+    public static async Task<string> RenderViewAsync<TModel>(
+        this Controller controller,
+        string viewName,
+        TModel model,
+        bool partial = false)
+    {
+        if (string.IsNullOrEmpty(viewName))
+        {
+            // Default: use the action name
+            viewName = controller.ControllerContext.ActionDescriptor.ActionName;
+        }
+
+        controller.ViewData.Model = model;
+
+        using (var writer = new StringWriter())
+        {
+            var serviceProvider = controller.HttpContext.RequestServices;
+            var engine = serviceProvider.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+            var viewResult = engine.FindView(controller.ControllerContext, viewName, !partial);
+
+            if (!viewResult.Success)
+            {
+                throw new FileNotFoundException($"View {viewName} not found.");
+            }
+
+            var viewContext = new ViewContext(
+                controller.ControllerContext,
+                viewResult.View,
+                controller.ViewData,
+                controller.TempData,
+                writer,
+                new HtmlHelperOptions()
+            );
+
+            await viewResult.View.RenderAsync(viewContext);
+            return writer.GetStringBuilder().ToString();
+        }
+    }
+}
