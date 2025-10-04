0271e33388e025be152e38881adb44339ce50bbb
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-10-04 17:39:57 +0530

    uploadexcel

--- eTactwebMasters/Controllers/ItemMasterController.cs
+++ eTactwebMasters/Controllers/ItemMasterController.cs
@@ -2356,6 +2356,39 @@ public class ItemMasterController : Controller
                                 }
                                    
                             }
+
+                            if (dbCol == "ItemType")  // <-- Special handling for ParentCode
+                            {
+                                string ItemCat = value.ToString().Trim();
+
+                                int ItemType = 0;
+                                var CatCode = _IItemMaster.GetItemCatCode(ItemCat);
+
+                                if (CatCode.Result.Result != null && CatCode.Result.Result.Rows.Count > 0)
+                                {
+                                    ItemType = (int)CatCode.Result.Result.Rows[0].ItemArray[0];
+                                }
+
+                                else
+                                {
+                                    ItemType = 0;
+                                }
+
+                                if (ItemType != 0)
+                                    value = ItemType;   // replace with code
+                                else
+                                {
+                                    return Json(new
+                                    {
+                                        StatusCode = 200,
+                                        StatusText = "Please Enter valid group"
+
+                                    });
+                                }
+
+                            }
+
+
                             if (columnType == typeof(int))
                                 value = int.Parse(value.ToString());
                             else if (columnType == typeof(decimal))
