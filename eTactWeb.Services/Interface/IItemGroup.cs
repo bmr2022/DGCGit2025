81784244b6679446f02b66da370383dce7b14e98
soumitra bhowmik <bhowmik_sumu@outlook.com>
2025-10-15 16:53:17 +0530

    importgroup change

--- eTactWeb.Services/Interface/IItemGroup.cs
+++ eTactWeb.Services/Interface/IItemGroup.cs
@@ -8,11 +8,10 @@ namespace eTactWeb.Services.Interface
         Task<ResponseResult> DeleteByID(int ID);
 
         Task<ItemGroupModel> GetByID(int ID);
-
+        Task<ResponseResult> GetItemCatCode(string CatCode);
         Task<ItemGroupModel> GetDashboardData(ItemGroupModel model);
-
         Task<IList<TextValue>> GetDropDownList(string Flag);
-
+        Task<ResponseResult> UpdateMultipleItemDataFromExcel(DataTable ItemDetailGrid, string flag);
         Task<ResponseResult> GetFormRights(int uId);
         Task<ResponseResult> GetUnderCategory(string Mode, string Type);
         Task<ResponseResult> CheckBeforeUpdate(int GroupCode);
