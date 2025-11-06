36409acdab5f7688d37d14fa90cc0c12363188f0
xon <bmr.client2021@gmail.com>
2025-11-06 23:40:50 +0530

    Employee advance payment form created

---
+++ eTactWeb.DOM/Models/HRAdvanceModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static eTactWeb.DOM.Models.Common;
+
+namespace eTactWeb.DOM.Models
+{
+    public class HRAdvanceModel : TimeStamp
+    {
+        public int AdvanceEntryId { get; set; }
+        public int AdvanceYearCode { get; set; }
+        public string AdvanceSlipNo { get; set; }
+        public int EmpId { get; set; }
+        public int EmpName { get; set; }
+        public int DesigId { get; set; }
+        public int DepId { get; set; }
+        public string DOJ { get; set; }
+        public decimal BasicSalary { get; set; }
+        public decimal NetSalary { get; set; }
+        public decimal PresentDaysinCurrMonth { get; set; }
+        public decimal PresentDaysInCurrYear { get; set; }
+        public int CategoryId { get; set; }
+        public string RequestDate { get; set; }
+        public string EntryDate { get; set; }
+        public string Purpose { get; set; }
+        public decimal RequestedAmount { get; set; }
+        public decimal ApprovedAmount { get; set; }
+        public decimal PreviousPendAdvanceAmt { get; set; }
+        public decimal PreviousPendLoanAmt { get; set; }
+        public string? MgrApprovaldate { get; set; }
+        public int? MgrApprovedbyEmpid { get; set; }
+        public int? HRApprovedbyEmpid { get; set; }
+        public string? HRApprovalDate { get; set; }
+        public int? FinanceApprovalEmpid { get; set; }
+        public string? FinanceApprovalDate { get; set; }
+        public int? CanceledByEmpId { get; set; }
+        public string? Canceled { get; set; }
+        public string? CancelOrApprovalremarks { get; set; }
+        public string? ModeOfPayment { get; set; }
+        public string PaymentReferenceNo { get; set; }
+        public string PaymentVoucherNo { get; set; }
+        public string PaymentVoucherType { get; set; }
+        public string PaymentRemark { get; set; }
+        public string RecoveryMethod { get; set; }
+        public int NoofInstallment { get; set; }
+        public string StartRecoveryFromMonth { get; set; }
+        public string AutoDeductionFromSalaryYN { get; set; }
+        public string? FinalRevoveryDate { get; set; }
+        public string? ActualFinalRecoveryDate { get; set; }
+        public string? StatusHoldCancelApproved { get; set; }
+        public int? LastUpdatedBy { get; set; }
+        public string? LastUpdatedByEmpName { get; set; }
+        public string? LastUpdationDate { get; set; }
+        public string RequestEntryByMachine { get; set; }
+        public string? ApprovedBYMachine { get; set; }
+        public string? CancelByMachine { get; set; }
+        public int? ActualEntryBy { get; set; }
+        public string? ActualEntryByName { get; set; }
+        public string CC { get; set; }
+        public int UID { get; set; }
+
+        public string FinFromDate { get; set;}
+        public string FinToDate { get; set; }
+    }
+}
