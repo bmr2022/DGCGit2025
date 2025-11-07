0d4c7511d876670c9be87f828393d0bbb645bf9e
xon <bmr.client2021@gmail.com>
2025-11-07 23:22:00 +0530

    Employee adance payment cchanges

---
+++ eTactWeb.Data/DAL/EmployeeAdvancePayementDAL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTactWeb.Data.Common;
+using eTactWeb.Services.Interface;
+using Microsoft.Extensions.Configuration;
+using static eTactWeb.DOM.Models.Common;
+
+namespace eTactWeb.Data.DAL
+{
+    public class EmployeeAdvancePayementDAL
+    {
+        private readonly IDataLogic _IDataLogic;
+        private readonly string DBConnectionString = string.Empty;
+        private readonly ConnectionStringService _connectionStringService;
+        private IDataReader? Reader;
+
+        public EmployeeAdvancePayementDAL(IConfiguration configuration, IDataLogic iDataLogic, ConnectionStringService connectionStringService)
+        {
+            _IDataLogic = iDataLogic;
+            _connectionStringService = connectionStringService;
+            DBConnectionString = _connectionStringService.GetConnectionString();
+        }
+
+        public async Task<ResponseResult> FillEntryId(int yearCode)
+        {
+            var _ResponseResult = new ResponseResult();
+            try
+            {
+                var SqlParams = new List<dynamic>();
+                SqlParams.Add(new SqlParameter("@Flag", "NewEntryId"));
+                SqlParams.Add(new SqlParameter("@AdvanceYearCode", yearCode));
+                _ResponseResult = await _IDataLogic.ExecuteDataTable("HRSPAdvanceMain", SqlParams);
+            }
+            catch (Exception ex)
+            {
+                dynamic Error = new ExpandoObject();
+                Error.Message = ex.Message;
+                Error.Source = ex.Source;
+            }
+
+            return _ResponseResult;
+        }
+    }
+}
