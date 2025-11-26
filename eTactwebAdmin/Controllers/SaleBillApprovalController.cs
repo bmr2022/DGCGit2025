26641247bb8952603b7d48c410209c2ff5016c48
xon <bmr.client2021@gmail.com>
2025-11-26 16:19:46 +0530

    SaleBillApproval Form (UI,Model,controller)

---
+++ eTactwebAdmin/Controllers/SaleBillApprovalController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using eTactWeb.DOM.Models;
+using DocumentFormat.OpenXml.EMMA;
+namespace eTactwebAdmin.Controllers
+{
+    public class SaleBillApprovalController : Controller
+    {
+
+        public IActionResult SaleBillApproval()
+        {
+            var MainModel = new SaleBillApprovalModel();
+            MainModel.FromDate = HttpContext.Session.GetString("FromDate");
+            MainModel.ToDate = HttpContext.Session.GetString("ToDate");
+
+            MainModel.ActualEntryId = Convert.ToInt32(HttpContext.Session.GetString("EmpID"));
+            MainModel.EmpId = Convert.ToInt32(HttpContext.Session.GetString("EmpID"));
+            MainModel.EntryByEmpName = HttpContext.Session.GetString("EmpName");
+            MainModel.ActualEntryDate = DateTime.Today.ToString("MM/dd/yyyy").Replace("-", "/");
+            MainModel.SaleBillList = new List<SaleBillApprovalDashboard>();
+
+            return View(MainModel);
+        }
+    }
+}
